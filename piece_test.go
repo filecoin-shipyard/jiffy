976d5640aafdb1fe25535f6c299e7436922a710a
Masih H. Derkani <m@derkani.org>
2023-08-24 19:42:02 +0100

    Fix failing test
    Fix test failing due to type mismatch.

--- piece_test.go
+++ piece_test.go
@@ -3,10 +3,11 @@ package jiffy
 import (
 	"testing"
 
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/stretchr/testify/require"
 )
 
 func TestEmptyHeaderV1SegmentSize(t *testing.T) {
 	require.NoError(t, emptyHeaderV1Segment.Info.Size.Validate())
-	require.Equal(t, emptyHeaderV1Segment.Info.Size, 128)
+	require.Equal(t, emptyHeaderV1Segment.Info.Size, abi.PaddedPieceSize(128))
 }
